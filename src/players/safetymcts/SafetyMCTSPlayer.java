46f75d7f5b5938a1d9f01f64e2ece740895c408c
Rohan Roy Burman <rrburman23@gmail.com>
2026-08-04 23:29:41 +0100

    Add Safety MCTS baseline agent

---
+++ src/players/safetymcts/SafetyMCTSPlayer.java
@@ -0,0 +1,99 @@
+package players.safetymcts;
+
+import core.GameState;
+import players.Player;
+import players.optimisers.ParameterizedPlayer;
+import utils.ElapsedCpuTimer;
+import utils.Types;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SafetyMCTSPlayer extends ParameterizedPlayer {
+
+    /**
+     * Random generator.
+     */
+    private Random m_rnd;
+
+    /**
+     * All actions available.
+     */
+    public Types.ACTIONS[] actions;
+
+    /**
+     * Params for this MCTS
+     */
+    public SafetyMCTSParams params;
+
+    public SafetyMCTSPlayer(long seed, int id) {
+        this(seed, id, new SafetyMCTSParams());
+    }
+
+    public SafetyMCTSPlayer(long seed, int id, SafetyMCTSParams params) {
+        super(seed, id, params);
+        reset(seed, id);
+
+        ArrayList<Types.ACTIONS> actionsList = Types.ACTIONS.all();
+        actions = new Types.ACTIONS[actionsList.size()];
+        int i = 0;
+        for (Types.ACTIONS act : actionsList) {
+            actions[i++] = act;
+        }
+    }
+
+    @Override
+    public void reset(long seed, int playerID) {
+        super.reset(seed, playerID);
+        m_rnd = new Random(seed);
+
+        this.params = (SafetyMCTSParams) getParameters();
+        if (this.params == null) {
+            this.params = new SafetyMCTSParams();
+            super.setParameters(this.params);
+        }
+    }
+
+    @Override
+    public Types.ACTIONS act(GameState gs) {
+
+        // TODO update gs
+        if (gs.getGameMode().equals(Types.GAME_MODE.TEAM_RADIO)){
+            int[] msg = gs.getMessage();
+        }
+
+        ElapsedCpuTimer ect = new ElapsedCpuTimer();
+        ect.setMaxTimeMillis(params.num_time);
+
+        // Number of actions available
+        int num_actions = actions.length;
+
+        // Root of the tree
+        SafetyTreeNode m_root = new SafetyTreeNode(params, m_rnd, num_actions, actions);
+        m_root.setRootGameState(gs);
+
+        //Determine the action using MCTS...
+        m_root.mctsSearch(ect);
+
+        //Determine the best action to take and return it.
+        int action = m_root.mostVisitedAction();
+
+        // TODO update message memory
+
+        //... and return it.
+        return actions[action];
+    }
+
+    @Override
+    public int[] getMessage() {
+        // default message
+        int[] message = new int[Types.MESSAGE_LENGTH];
+        message[0] = 1;
+        return message;
+    }
+
+    @Override
+    public Player copy() {
+        return new SafetyMCTSPlayer(seed, playerID, params);
+    }
+}
